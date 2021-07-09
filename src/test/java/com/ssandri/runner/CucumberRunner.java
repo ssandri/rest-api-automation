8bb9056e857943ddfb59cefb89fa1faa09cf0770
Sebastian Sandri <sebastianmsandri@gmail.com>
2021-07-09 20:20:38 -0300

    Added parallel execution by TestNG

--- src/test/java/com/ssandri/runner/CucumberRunner.java
+++ src/test/java/com/ssandri/runner/CucumberRunner.java
@@ -2,6 +2,7 @@ package com.ssandri.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 @Test
@@ -15,4 +16,9 @@ import org.testng.annotations.Test;
     })
 public class CucumberRunner extends AbstractTestNGCucumberTests {
 
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
